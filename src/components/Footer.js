7cee2eac4a73c882072631c6bc046dbea23fedc6
Arun Kumar <atulkumar3113@gmail.com>
2020-05-26 21:16:28 +0530

    social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,29 @@ const Footer = () => {
     <div className="footerLinks m-4">
       <div className="text-right d-flex justify-content-center">
         <button>
-          <a href="/d">
+          <a
+            href="https://www.facebook.com/kumar.atul.18400"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fa fa-facebook"></i>
           </a>
         </button>
         <button>
-          <a href="/sdf">
+          <a
+            href="https://www.instagram.com/Kumarak20/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fa fa-instagram"></i>
           </a>
         </button>
         <button>
-          <a href="/sb">
+          <a
+            href="https://dribbble.com/imAtul20"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fa fa-dribbble"></i>
           </a>
         </button>
