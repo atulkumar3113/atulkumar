52700fc4f851617b9908dc41af266553e0422ac1
Arun Kumar <atulkumar3113@gmail.com>
2020-05-26 11:11:20 +0530

    grid

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ const Navbar = () => {
     <div className="links">
       <div className="text-right d-flex justify-content-md-end justify-content-center">
         <button>Home</button>
-        <button>My Work</button>
+        <button>
+          <a href="#work">My Work</a>
+        </button>
         <button>About</button>
         <button>Contact</button>
       </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,70 +1,28 @@
-import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import React, { useState } from "react";
 import "../styles/work.css";
 
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4,
-  },
-  tablet: {
-    breakpoint: { max: 1224, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 598, min: 0 },
-    items: 1,
-  },
-};
-
 const Work = () => {
+  const [ImgSource] = useState([
+    "http://placekitten.com/320/220",
+    "http://placekitten.com/320/221",
+    "http://placekitten.com/320/222",
+    "http://placekitten.com/320/220",
+    "http://placekitten.com/320/221",
+    "http://placekitten.com/320/222",
+    "http://placekitten.com/320/220",
+    "http://placekitten.com/320/221",
+    "http://placekitten.com/320/222",
+  ]);
   return (
-    <div className="recentWork">
+    <div id="work" className="recentWork">
       <h6>Recent Works</h6>
-      <Carousel
-        swipeable={true}
-        draggable={false}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={100}
-        containerClass="carousel-container"
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        <div className="img-box">
-          <img
-            src="https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg"
-            alt=""
-          />
-        </div>
-        <div className="img-box">
-          <img
-            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-            alt=""
-          />
-        </div>
-        <div className="img-box">
-          <img
-            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-            alt=""
-          />
-        </div>
-        <div className="img-box">
-          <img
-            src="https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg"
-            alt=""
-          />
-        </div>
-      </Carousel>
+      <div className="gridLayout">
+        {ImgSource.map((item, index) => (
+          <div className="img-wrapper" key={index}>
+            <img src={item} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
